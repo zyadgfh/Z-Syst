f72a50cc688e704f354b6d65ae0a014cfc6613f6
zyadgfh <zyadgamal796@gmail.com>
2026-09-19 19:57:20 +0300

    fix: correct localization provider alias after rename

--- pharmacy-store-app-codecanyon-main/lib/Screens/Income/add_income.dart
+++ pharmacy-store-app-codecanyon-main/lib/Screens/Income/add_income.dart
@@ -102,7 +102,7 @@ class _AddIncomeState extends State<AddIncome> {
     selectedPaymentType ??= paymentMethods.first;
     print(paymentMethods);
     final theme = Theme.of(context);
-    final l10n = l10n.S.of(context);
+    final l10n = lang.S.of(context);
     return Consumer(
       builder: (context, ref, __) {
         final data = ref.watch(incomeCategoryProvider);
