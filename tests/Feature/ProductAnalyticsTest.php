2fbe9d9124032e4f9788798bf02420f33b1f19d3
zyadgfh <zyadgamal796@gmail.com>
2026-09-22 14:55:39 +0300

    test: protect product analytics endpoint

---
+++ tests/Feature/ProductAnalyticsTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ProductAnalyticsTest extends TestCase
+{
+    public function test_analytics_endpoint_requires_authentication(): void
+    {
+        $this->getJson('/api/v1/product-analytics?period=month')->assertUnauthorized();
+    }
+}
