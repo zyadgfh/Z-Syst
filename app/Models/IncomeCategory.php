1817ac3f5bf59454433028c6579a6f7ed72fb5ef
zyadgfh <zyadgamal796@gmail.com>
2026-09-26 18:59:40 +0300

    security: apply centralized tenant isolation to IncomeCategory

--- app/Models/IncomeCategory.php
+++ app/Models/IncomeCategory.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Concerns\BelongsToBusiness;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class IncomeCategory extends Model
 {
-    use HasFactory;
+    use HasFactory, BelongsToBusiness;
 
     protected $fillable = [
         'categoryName',
