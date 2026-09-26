03ad7fc600693f1d0f2ac549a238428af743db38
zyadgfh <zyadgamal796@gmail.com>
2026-09-26 18:56:31 +0300

    security: apply centralized tenant isolation to BoxSize

--- app/Models/BoxSize.php
+++ app/Models/BoxSize.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Concerns\BelongsToBusiness;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class BoxSize extends Model
 {
-    use HasFactory;
+    use HasFactory, BelongsToBusiness;
 
     protected $fillable = [
         'name',
