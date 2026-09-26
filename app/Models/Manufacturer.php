de966c80ba1f7f0a426ffb08a85678c7d73acb21
zyadgfh <zyadgamal796@gmail.com>
2026-09-26 18:56:25 +0300

    security: apply centralized tenant isolation to Manufacturer

--- app/Models/Manufacturer.php
+++ app/Models/Manufacturer.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Concerns\BelongsToBusiness;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Manufacturer extends Model
 {
-    use HasFactory;
+    use HasFactory, BelongsToBusiness;
 
     protected $fillable = [
         'name',
