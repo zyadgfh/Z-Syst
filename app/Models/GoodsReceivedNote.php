69e685c0ba6da8b7892dce738b042753d433dbb3
zyadgfh <zyadgamal796@gmail.com>
2026-09-26 18:57:42 +0300

    security: apply centralized tenant isolation to GoodsReceivedNote

--- app/Models/GoodsReceivedNote.php
+++ app/Models/GoodsReceivedNote.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Concerns\BelongsToBusiness;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -10,6 +12,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class GoodsReceivedNote extends Model
 {
+    use BelongsToBusiness;
     use HasFactory, SoftDeletes;
 
     /**
