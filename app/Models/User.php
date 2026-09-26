e90bb97ad624b91373528eceb90be25bfb222e2c
zyadgfh <zyadgamal796@gmail.com>
2026-09-26 18:32:33 +0300

    security: prevent user business reassignment via mass assignment

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,13 +13,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, HasRoles, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
-        'business_id',
         'name',
         'role',
         'email',
@@ -33,36 +27,24 @@ class User extends Authenticatable
         'email_verified_at',
     ];
 
-    /**
-     * The "booted" method of the model.
-     */
     protected static function booted()
     {
-        // Only apply tenant scope in non-admin contexts
         static::addGlobalScope('tenant', function ($query) {
-            if (auth()->check() && 
-                auth()->user()->role !== 'superadmin' && 
-                !request()->is('admin/*')) {
+            if (
+                auth()->check() &&
+                auth()->user()->role !== 'superadmin' &&
+                !request()->is('admin/*')
+            ) {
                 $query->where('business_id', auth()->user()->business_id);
             }
         });
     }
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'password' => 'hashed',
         'visibility' => 'json',
